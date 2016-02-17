85b206d0b7b04ff0b462f90d507c581fe4e6f4a5
Patka <epicpatka@gmail.com>
2016-02-16 20:25:23 -0500

    Providence Canyon, Balanced Rock, Corvette Museum
    new content
    -Providence Canyon
    -Balanced Rock
    -Corvette Museum
    -Country flag images for USA and Poland
    
    performance
    -removed unnecessary render-blocking css and js
    -new USA map controls
    -increase sidebar height from 80 to 90 vh
    -moved state borders to separate file
    -removed geocoder test code
    -removed test code for Brazil and Denmark

--- geography/assets/content.js
+++ geography/assets/content.js
@@ -3,42 +3,31 @@ var usaString = '<div id="content">'+
 '<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
 '<h1 id="firstHeading" class="firstHeading">USA</h1>'+
 '<div id="bodyContent">'+
+'<div id="q_opt" class="btn-group" data-toggle="buttons" align="center">'+
+'<label class="btn btn-default" id="radio_regions">'+
+	'<input id="radio_regions" name="op" type="radio" value="0" />Regions</label>'+
+'<label class="btn btn-default" id="radio_red_blue">'+
+	'<input id="radio_red_blue" name="op" type="radio" value="1" />Red / Blue States</label>'+
+'<label class="btn btn-default" id="radio_population">'+
+	'<input id="radio_population" name="op" type="radio" value="2" />Show Population</label>'+
+'<label class="btn btn-default" id="radio_clear">'+
+	'<input id="radio_clear" name="op" type="radio" value="3" />Clear</label><br><br>'+
+'</div>'+
+'<img src="assets/images/american_flag.jpg" class="img_flag">'+
 '<h2>Regions</h2>'+
 '<p>Northeast - Red<br>Southeast - Green<br>Midwest - Blue<br>Southwest - Yellow<br>West - Purple</p>'+
-
-'<div id="q_opt" class="btn-group" data-toggle="buttons">'+
-'<label class="btn btn-default active" id="radio_regions">'+
-	'<input id="radio_regions" name="op" type="radio" value="0" />Regions</label><br>'+
-'<label class="btn btn-default" id="radio_red_blue">'+
-	'<input id="radio_red_blue" name="op" type="radio" value="1" />Red / Blue States</label><br>'+
 '</div>'+
-'<label class="btn btn-default" id="radio_population">'+
-	'<input id="radio_population" name="op" type="radio" value="2" />Show Population</label><br>'+
-'</div>'+
-'<label class="btn btn-default" id="radio_clear">'+
-	'<input id="radio_clear" name="op" type="radio" value="3" />Clear</label><br>'+
-'</div>'+
-
 '</div>'+
 '</div>';
 
-var brazilString = '<div id="content">'+
+var polandString = '<div id="content">'+
 '<div id="siteNotice">'+
 '<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
-'<h1 id="firstHeading" class="firstHeading">Brazil</h1>'+
+'<h1 id="firstHeading" class="firstHeading">Poland</h1>'+
 '<div id="bodyContent">'+
-'<p align="left"><b>ok</b>'+
-'test</p>'+
-'</div>'+
-'</div>';
-
-var denmarkString = '<div id="content">'+
-'<div id="siteNotice">'+
-'<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
-'<h1 id="firstHeading" class="firstHeading">Denmark</h1>'+
-'<div id="bodyContent">'+
-'<p align="left"><b>hello</b>'+
-'test</p>'+
+'<img src="assets/images/poland_flag.svg" class="img_flag">'+
+'<p align="left"><b>I am from Poland!</b>'+
+'</p>'+
 '</div>'+
 '</div>';
 
@@ -96,6 +85,39 @@ var hearstCastleString = '<div id="content">'+
 '<div id="bodyContent">'+
 '<iframe class="imgStreet" width="1000" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyAxmMoaV9GrSom0cLblARIad6quTrkSSt0&location=35.6853182,-121.169027&heading=75&pitch=15&fov=35"></iframe>'+
 '<p align="left">The Hearst Castle is located on the Central Cost of California. Architect Julia Morgan designed the castle for William Randolph Hearst from 1919 to 1947. The castle has a wine cellar which protected Hearst\'s collection of alcohol during the prohibition era. There is also an airport which was built in the 1920s so that Hearst\'s famous guests could get to his estate faster than by train or car. There used to be the world\'s largest private zoo with zebras, aoudads, and Sambar deer. There are two pools at the castle: the Neptune Pool and the Roman Pool. Originally, the Neptune pool was supposed to be small and decorated with surrounding ornaments. However, on March 31, 1924, William Randolph Hearst wrote a letter to Julia Morgan and said that his wife and children want the pool to be as large as a swimming pool and to disregard the temple structure. The indoor Roman Pool has 205,000 gallons of water and a depth of 10 feet.'+
+'</p>'+
+'</div>'+
+'</div>';
+
+var providenceCanyonString = '<div id="content">'+
+'<div id="siteNotice">'+
+'<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
+'<h1 id="firstHeading" class="firstHeading">Providence Canyon</h1>'+
+'<div id="bodyContent">'+
+'<iframe class="imgStreet" width="1000" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyAxmMoaV9GrSom0cLblARIad6quTrkSSt0&location=32.0688,-84.9076958&heading=-120&pitch=15&fov=100"></iframe>'+
+'<p align="left">The Providence Canyon in Georgia is nicknamed the "little grand canyon." The canyon was formed by erosion of colorful soft soil due to poor farming in the 1800s. Formed during the Cretaceous Period, the sandstone bluffs are one of the oldest rock formations in Georgia. The canyon has the largest known wild colony of plumleaf azalea, rare flowers from southwestern Georgia and eastern Alabama, which bloom in mid to late summer.'+
+'</p>'+
+'</div>'+
+'</div>';
+
+var balancedRockString = '<div id="content">'+
+'<div id="siteNotice">'+
+'<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
+'<h1 id="firstHeading" class="firstHeading">Balanced Rock County</h1>'+
+'<div id="bodyContent">'+
+'<iframe class="imgStreet" width="1000" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyAxmMoaV9GrSom0cLblARIad6quTrkSSt0&location=38.7010115,-109.5652711&heading=100.56&pitch=15&fov=100"></iframe>'+
+'<p align="left">The mountain is approximately 128 feet (39 meters) tall. The balancing rock rises about 55 feet (16.75 meters) above the base of the mountain. The biggest rock is as big as three school buses.'+
+'</p>'+
+'</div>'+
+'</div>';
+
+var nationalCorvetteMuseumString = '<div id="content">'+
+'<div id="siteNotice">'+
+'<a onclick=$(".sidebar.bottom").trigger("sidebar:close");><i class="fa fa-times pull-right"></i></a></div>'+
+'<h1 id="firstHeading" class="firstHeading">National Corvette Museum</h1>'+
+'<div id="bodyContent">'+
+'<iframe class="imgStreet" width="1000" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyAxmMoaV9GrSom0cLblARIad6quTrkSSt0&location=37.0044626,-86.3748681&heading=187&pitch=15&fov=100"></iframe>'+
+'<p align="left">An indoor museum that tells the history of Corvette. Corvette models ranging from the 1950s to today are featured.'+
 '</p>'+
 '</div>'+
 '</div>';
